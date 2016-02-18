57abfab51d764167edbcedebcf5893fbb6a1f96f
Rodolfo Carvalho <rhcarvalho@gmail.com>
2016-02-18 16:04:53 +0100

    Add func to run commands in a concurrent pipeline

--- redgreen/redgreen.go
+++ redgreen/redgreen.go
@@ -6,6 +6,41 @@ import (
 	"time"
 )
 
+// RunSpec holds the specification of a command to be run.
+type RunSpec struct {
+	Command []string
+	Timeout time.Duration
+}
+
+// Run runs commands coming from the in channel in a new goroutine and returns a
+// channel of errors of each execution. Input and output is synchronized, a new
+// command will be executed only after the error returned by the previous
+// execution is consumed downstream. Closing either done or in signals that no
+// more commands are to be run, and, consequently, the output channel will be
+// closed.
+func Run(done <-chan struct{}, in <-chan RunSpec) <-chan error {
+	out := make(chan error)
+	go func() {
+		defer close(out)
+		for {
+			select {
+			case spec, ok := <-in:
+				if !ok {
+					return
+				}
+				select {
+				case out <- run(spec.Command, spec.Timeout):
+				case <-done:
+					return
+				}
+			case <-done:
+				return
+			}
+		}
+	}()
+	return out
+}
+
 // run runs command and waits for it to terminate for at most timeout. Zero or
 // negative timeout means no timeout.
 func run(command []string, timeout time.Duration) error {
--- redgreen/redgreen_test.go
+++ redgreen/redgreen_test.go
@@ -9,6 +9,95 @@ import (
 	"time"
 )
 
+func TestRunDoneBlockedOnIn(t *testing.T) {
+	done := make(chan struct{})
+	in := make(chan RunSpec)
+	out := Run(done, in)
+
+	// Test the execution of a single command.
+	in <- RunSpec{Command: []string{"true"}}
+	if err := <-out; err != nil {
+		t.Fatalf("got %#v, want nil", err)
+	}
+
+	// There is no more input, Run's goroutine should be blocked on
+	// receiving from in.
+
+	// Signal that we are done.
+	close(done)
+
+	// Ensure that out is eventually closed.
+	mustBeClosedTimeout(out, time.Second, t)
+}
+
+func TestRunDoneBlockedOnOut(t *testing.T) {
+	var outWasOpen, outWasClosed bool
+	test := func() {
+		done := make(chan struct{})
+		in := make(chan RunSpec)
+		out := Run(done, in)
+
+		// Send a single command but don't receive from out, so that
+		// Run's goroutine will be blocked on sending to out.
+		in <- RunSpec{Command: []string{"true"}}
+
+		// Signal that we are done.
+		close(done)
+
+		// Ensure that out is eventually closed.
+		timeout := time.Second
+		select {
+		case _, isOpen := <-out:
+			// Since we receive from out here, we can observe that
+			// the receive operation either succeed or not with
+			// equal probability.
+			if isOpen {
+				outWasOpen = true
+			} else {
+				outWasClosed = true
+			}
+		case <-time.After(timeout):
+			t.Fatalf("receive from channel timed out")
+		}
+		mustBeClosedTimeout(out, timeout, t)
+	}
+	var runs int
+	for !(outWasOpen && outWasClosed) {
+		// 2 or 3 runs should be enough. The probability of needing more
+		// than 16 runs to observe both outWasOpen and outWasClosed is
+		// (1/2)^16 ≈ 0.001
+		if runs > 16 {
+			t.Fatalf("failed to observe both conditions: outWasOpen=%v, outWasClosed=%v", outWasOpen, outWasClosed)
+		}
+		test()
+		runs++
+	}
+	t.Logf("#runs: %d", runs)
+}
+
+func TestRunClosedInput(t *testing.T) {
+	done := make(chan struct{})
+	in := make(chan RunSpec)
+	out := Run(done, in)
+
+	// Signal that there will be no more input.
+	close(in)
+
+	// Ensure that out is eventually closed.
+	mustBeClosedTimeout(out, time.Second, t)
+}
+
+func mustBeClosedTimeout(ch <-chan error, timeout time.Duration, t *testing.T) {
+	select {
+	case _, isOpen := <-ch:
+		if isOpen {
+			t.Fatalf("channel is open, want closed")
+		}
+	case <-time.After(timeout):
+		t.Fatalf("receive from channel timed out")
+	}
+}
+
 func Test_run(t *testing.T) {
 	tests := []struct {
 		command []string
