d4bf69ee2e3dd2a8c134347eb4acffac4a564abe
Rodolfo Carvalho <rhcarvalho@gmail.com>
2016-02-21 02:46:24 +0100

    Rewrite redgreen

---
+++ main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/nsf/termbox-go"
+	"github.com/rhcarvalho/redgreen/redgreen"
+)
+
+// Command-line flags and arguments.
+var (
+	testCommand = []string{"go", "test"}
+	timeout     time.Duration
+	debug       bool
+)
+
+func init() {
+	flag.BoolVar(&debug, "debug", false, "enable debug mode, disable termbox")
+	flag.DurationVar(&timeout, "timeout", 5*time.Second, "maximum time to wait for command to finish")
+}
+
+func main() {
+	flag.Parse()
+
+	// Customize testCommand if passed as arguments.
+	args := flag.Args()
+	if len(args) > 0 {
+		testCommand = args
+	}
+
+	if err := do(); err != nil {
+		log.Fatalf("ERROR: %v", err)
+	}
+}
+
+func do() error {
+	done := make(chan struct{})
+	defer close(done)
+
+	if !debug {
+		if err := termbox.Init(); err != nil {
+			return err
+		}
+		defer termbox.Close()
+		termbox.HideCursor()
+		termbox.SetOutputMode(termbox.Output256)
+	}
+
+	w, err := redgreen.Watch(done, ".")
+	if err != nil {
+		return err
+	}
+
+	runSpec := redgreen.RunSpec{Command: testCommand, Timeout: timeout}
+
+	run := make(chan redgreen.RunSpec, 1)
+	// Run test command at least once.
+	run <- runSpec
+	// Run tests every time a file is created/removed/modified.
+	go func() {
+		for range w {
+			run <- runSpec
+		}
+	}()
+
+	res := redgreen.Run(done, run)
+
+	state := make(chan redgreen.State)
+	go redgreen.Render(done, state)
+
+	s := redgreen.State{Debug: debug}
+	// Render initial state.
+	state <- s
+	// Render after every test command result.
+	go func() {
+		for r := range res {
+			s.Results = append(s.Results, r)
+			state <- s
+		}
+	}()
+
+	if debug {
+		// Wait for Ctrl-C.
+		ch := make(chan os.Signal, 1)
+		signal.Notify(ch, os.Interrupt)
+		<-ch
+	} else {
+		// Block until Esc is pressed.
+		for {
+			e := termbox.PollEvent()
+			if e.Type == termbox.EventKey && e.Key == termbox.KeyEsc {
+				break
+			}
+		}
+	}
+	return nil
+}
